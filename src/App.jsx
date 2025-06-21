bedb824e15190e6212fdba3b830e0041acd2c3eb
Abu Al Mahadi <mohammed.abumahadi@northsouth.edu>
2025-06-21 23:23:28 +0600

    favorites, movies, api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import NavBar from './components/NavBar'
-import {Routes, Route} from "react-router-dom"
-import './css/App.css'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar/>
-      <main className='main-content'>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
-
-  )
+    </MovieProvider>
+  );
 }
 
 export default App;
